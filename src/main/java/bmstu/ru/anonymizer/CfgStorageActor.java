6edbf9a3ec3bd6c2885910fabad03ddc3b4ddd52
Tarasov Alexey <tarasovalx@gmail.com>
2021-12-17 22:37:00 +0100

    Scripted auto-commit on change (2021-12-17 22:37:08) by gitwatch.sh

--- src/main/java/bmstu/ru/anonymizer/CfgStorageActor.java
+++ src/main/java/bmstu/ru/anonymizer/CfgStorageActor.java
@@ -3,6 +3,7 @@ package bmstu.ru.anonymizer;
 import akka.actor.AbstractActor;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class CfgStorageActor extends AbstractActor {
     private ArrayList<String> data = new ArrayList<>();
@@ -14,5 +15,11 @@ public class CfgStorageActor extends AbstractActor {
     private void saveServerList(ServerList list) {
         data = new ArrayList<>(list.getData());
         System.out.println(data);
+    }
+
+    private void redirect(ServerRequest request) {
+        Random rnd = new Random();
+        int target = rnd.nextInt(data.size());
+        sender().tell(data.get(target), getSelf());
     }
 }
