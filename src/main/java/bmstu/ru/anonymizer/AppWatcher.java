332c61916a3e06ce42409592bca369a13e5b23c2
Tarasov Alexey <tarasovalx@gmail.com>
2021-12-17 20:21:00 +0100

    Scripted auto-commit on change (2021-12-17 20:21:37) by gitwatch.sh

--- src/main/java/bmstu/ru/anonymizer/AppWatcher.java
+++ src/main/java/bmstu/ru/anonymizer/AppWatcher.java
@@ -1,4 +1,25 @@
 package bmstu.ru.anonymizer;
 
-public class AppWatcher {
+import akka.actor.ActorRef;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+public class AppWatcher implements Watcher {
+    private final ActorRef config;
+    private ZooKeeper zk;
+    private static final String ZK_SERVERS_PATH = "/servers";
+
+    public AppWatcher(ActorRef config) {
+        this.config = config;
+    }
+
+    public AppWatcher(ActorRef config, ZooKeeper zk) {
+        this.config = config;
+        this.zk = zk;
+    }
+
+    public void process(WatchedEvent event) {
+
+    }
 }
