9d91b95340fdb6eacdb521bcbbe02f5bfb3fcf68
Tarasov Alexey <tarasovalx@gmail.com>
2021-12-17 19:08:00 +0100

    Scripted auto-commit on change (2021-12-17 19:08:22) by gitwatch.sh

--- src/main/java/bmstu/ru/anonymizer/AppNode.java
+++ src/main/java/bmstu/ru/anonymizer/AppNode.java
@@ -1,4 +1,52 @@
 package bmstu.ru.anonymizer;
 
-public class AppNode {
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+
+import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
+
+import java.time.Duration;
+
+public class AppNode extends AllDirectives {
+
+    private static final String HOSTNAME = "localhost";
+
+    private static final String ZK_ADDRESS = "127.0.0.1:2181";
+    private static final int ZK_TIMEOUT = 3000;
+    private static final int TIMEOUT = 5000;
+    private static final String ZK_PATH = "\"/servers/s\"";
+
+
+    private static ActorRef config;
+    private static Integer port;
+    private static ActorSystem system;
+    private static AppWatcher watcher;
+
+    private Route createRoute(ActorSystem system) {
+        return route(
+                get()
+        );
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    private Route get() {
+        return parameter("url", url ->
+                        parameter("count", count -> {
+                            int counter = Integer.parseInt(count);
+                            final Http http = Http.get(system);
+                            if (counter == 0) {
+                                return completeWithFuture(http.singleRequest(HttpRequest.create(url)));
+                            }
+
+
+                        })
+        );
+    }
 }
